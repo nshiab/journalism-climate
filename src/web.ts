105187afd4e24d8b85e8217cc4cf00f1a10a4f03
nshiab <nael.shiab@gmail.com>
2025-12-19 09:26:50 -0500

    Updating doc

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,12 @@
 /**
+ * @module
+ *
  * This module provides a collection of functions to be used in web applications.
  *
  * To import a function, use:
  * ```ts
  * import { functionName } from "@nshiab/journalism-climate/web";
  * ```
- *
- * @module
  */
 
 import getHumidex from "./climate/getHumidex.ts";
